3fc9d3359dfac8847068f5342e9e4d88201b7938
gpower-crypto <72075399+gpower-crypto@users.noreply.github.com>
2021-04-06 15:35:33 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const things= require("./routes/things");
 
 const app = express();
+
+app.use("/things", things);
+
 app.use(bodyParser.urlencoded({
   extended: true
-})); //bodyParser will parse or analyse the http request in line 15,16 and
+})) //bodyParser will parse or analyse the http request in line 15,16 and
 //urlencoded will extract the values(form data) entered in form.
+app
+  .route("/")
+  .get(function(req, res) { //get request is ment to get files from the server
+    res.sendFile(__dirname + "/index.html");
+  }) //__dirname- used so thtat it doesn't matter where the server is, it will pend the index.html file and give its locatin.
 
-app.get("/", function(req, res) { //fet request is ment to get files from the server
-  res.sendFile(__dirname + "/index.html");
-}); //__dirname- used so thtat it doesn't matter where the server is, it will pend the index.html file and give its locatin.
-
-app.post("/", function(req, res) { //http post request is when the browser posts/gives data to the server that the user has entered.
+  .post(function(req, res) { //http post request is when the browser posts/gives data to the server that the user has entered.
   //When the user posts anything, what function should take place.
   console.log(req.body);
   console.log(req.body);
@@ -21,9 +26,10 @@ app.post("/", function(req, res) { //http post request is when the browser posts
 
   var result = number1 + number2;
 
-  res.send("The result is:" + result);//display the message on the user's screen.
+  res.send("The result is:" + result); //display the message on the user's screen.
 });
 
+
 app.listen(3000, function() {
-  console.log("server started on port 3000")//server is channeled to port 3000.
+  console.log("server started on port 3000") //server is channeled to port 3000.
 });
